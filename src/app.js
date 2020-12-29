5af57741f9229c39e9cecf8ba26cb83c34c8b7e6
Toxa-p07a1330 <p07a1330@yandex.ru>
2020-12-29 17:02:12 +0300

    error demo

--- src/app.js
+++ src/app.js
@@ -1,15 +1,31 @@
+const fetch = require("node-fetch");
 console.log("XML to JSON converter")
 let xmlString = "";
-let getData = ()=>{
+let getData = async ()=>{
     xmlString = "someXML";
-    fe
+    fetch("https://raw.githubusercontent.com/Toxa-p07a1330/XMLtoJSONConverter/master/XMLExample").then(
+        (response)=>{
+            response.text().then(
+                (text)=>{
+                    xmlString = text;
+                    return text;
+                },
+                (reason) =>{
+                    console.log(reason)
+                })
+        },
+        (reject)=>{
+            console.log(reject)
+        }
+    )
 }
-let parceXmlToObject= ()=>{
+let parceXmlToObject= async ()=>{
 
 }
 
 async function xmlConvertJson() {
-
-
+let test = await getData();
+console.log(test)
 }
 xmlConvertJson();
+
