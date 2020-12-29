b070afb97fadfd68129033bcf2653bc309a908ed
Toxa-p07a1330 <p07a1330@yandex.ru>
2020-12-29 16:03:12 +0300

    base

---
+++ src/app.js
@@ -0,0 +1,15 @@
+console.log("XML to JSON converter")
+let xmlString = "";
+let getData = ()=>{
+    xmlString = "someXML";
+    fe
+}
+let parceXmlToObject= ()=>{
+
+}
+
+async function xmlConvertJson() {
+
+
+}
+xmlConvertJson();
