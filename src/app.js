59f001dc9b250ccf1c433f0ddf9159f9bc8467d7
Toxa-p07a1330 <p07a1330@yandex.ru>
2020-12-31 00:02:11 +0300

    Works)

--- src/app.js
+++ src/app.js
@@ -1,19 +1,54 @@
 const fetch = require("node-fetch");
-console.log("XML to JSON converter")
-let xmlString = "";
-let getData = async ()=>{
-    xmlString = "someXML";
-    let rezult = await fetch("https://raw.githubusercontent.com/Toxa-p07a1330/XMLtoJSONConverter/master/XMLExample");
-    rezult = await rezult.text();
-    xmlString = rezult;
-    return rezult
+let studentsJson = "";
+let wayToFile = "https://raw.githubusercontent.com/Toxa-p07a1330/XMLtoJSONConverter/master/students.json"
+let fields = {
+    name: 15,
+    group: 8,
+    avg:6,
+    debt: 18
 }
-
+let titles = Object.keys(fields);
+let delim = "|---------------|--------|------|------------------|";
+let getData = async ()=>{
+    studentsJson = "students";
+    let rezult = await fetch(wayToFile);
+    rezult = await rezult.json();
+    studentsJson = rezult;
+}
 
 async function parse(){
-    let sem = await getData();
-    parser = new DOMParser();
-    xmlDoc = parser.parseFromString(xmlString, "text/xml");
-    console.log(xmlDoc)
+    await getData();
+    if (!(studentsJson.items instanceof Array)){
+        console.log("Field of items is not array")
+        return;
+    }
+    if (studentsJson._meta.count !== studentsJson.items.length){
+        console.log("Not matching array size")
+        return;
+    }
+    console.log("File is correct")
+    let outputStr = "|";
+    let head = titles.map((value, index, array)=>{
+        outputStr+=value;
+        for (let i =0; i<fields[value]-value.length; i++)
+            outputStr+=" ";
+        outputStr+="|";
+        return outputStr;
+    })[titles.length-1]
+    console.log(head)
+    console.log(delim)
+    studentsJson.items.map((value)=>{
+        let row = "|";
+        let row_out = titles.map((valueTitle)=>{
+           row+=value[valueTitle];
+           let spaces = fields[valueTitle]-(value[valueTitle]+"").length
+            for (let i =0; i<spaces; i++)
+                row+=" ";
+            row+="|";
+           return row;
+        })[titles.length-1]
+        console.log(row_out)
+        console.log(delim)
+    })
 }
-parse();
+parse()
